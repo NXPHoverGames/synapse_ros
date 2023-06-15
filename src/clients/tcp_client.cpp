f02cd13582de27e617192a466bf4a5fcce1f36a3
James Goppert <james.goppert@gmail.com>
2023-06-15 00:44:12 +0000

    Add socket reconnect.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- src/clients/tcp_client.cpp
+++ src/clients/tcp_client.cpp
@@ -7,6 +7,7 @@
 #include "synapse_protobuf/odometry.pb.h"
 
 #include "synapse_tinyframe/SynapseTopics.h"
+#include <boost/asio/error.hpp>
 #include <boost/date_time/posix_time/posix_time_config.hpp>
 #include <boost/date_time/posix_time/posix_time_duration.hpp>
 #include <boost/system/error_code.hpp>
@@ -49,7 +50,7 @@ void TcpClient::handle_connect(
         }
     } else {
         connected_ = true;
-        std::cout << "tcp connnected: " << endpoint << std::endl;
+        std::cout << "tcp connected: " << endpoint << std::endl;
     }
 }
 
@@ -78,15 +79,26 @@ void TcpClient::tick(const boost::system::error_code& /*e*/) {
 
 void TcpClient::tx_handler(const boost::system::error_code & ec, std::size_t bytes_transferred) {
     (void)bytes_transferred;
-    if (ec != boost::system::errc::success) {
+    if (ec == boost::asio::error::eof) {
+        std::cerr << "reconnecting due to eof" << std::endl;
+        connected_ = false;
+    } else if (ec == boost::asio::error::connection_reset) {
+        std::cerr << "reconnecting due to reset" << std::endl;
+        connected_ = false;
+    } else if (ec != boost::system::errc::success) {
         std::cerr << "tx error: " << ec.message() << std::endl;
     }
 }
 
 void TcpClient::rx_handler(const boost::system::error_code & ec, std::size_t bytes_transferred) {
-    if (ec != boost::system::errc::success) {
+    if (ec == boost::asio::error::eof) {
+        std::cerr << "reconnecting due to eof" << std::endl;
+        connected_ = false;
+    } else if (ec == boost::asio::error::connection_reset) {
+        std::cerr << "reconnecting due to reset" << std::endl;
+        connected_ = false;
+    } else if (ec != boost::system::errc::success) {
         std::cerr << "rx error: " << ec.message() << std::endl;
-        return;
     }
     const std::lock_guard<std::mutex> lock(guard_rx_buf_);
     TF_Accept(tf_.get(), rx_buf_, bytes_transferred);
@@ -153,7 +165,8 @@ void TcpClient::run_for(std::chrono::seconds sec) {
 void TcpClient::write(const uint8_t *buf, uint32_t len)
 {
     if (connected_) {
-        boost::asio::async_write(sockfd_, boost::asio::buffer(buf, len), TcpClient::tx_handler);
+        boost::asio::async_write(sockfd_, boost::asio::buffer(buf, len), 
+                std::bind(&TcpClient::tx_handler, this, _1, _2));
     }
 }
 
